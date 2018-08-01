0087a90af7218205f8969b9c3ea626c0f89a8e3e
Naozumi Hiranuma <hiranumn@gmail.com>
2018-08-01 14:17:05 -0700

    MatrixReader can do both UInt32 and UInt64 now. Also wrote unit test for it.

--- src/MatrixReader.jl
+++ src/MatrixReader.jl
@@ -4,47 +4,73 @@ export MatrixWriter, close, writeMatrix, MatrixReader, value, eof, advance!, mea
 type MatrixWriter
     fileStream
     zerocount::Int
-    binsize::Int
+    maxnum::Int ##Somewhat of unused number
     expsize::Int
+    datatype::DataType
 end
 
-function MatrixWriter(fileName::String, expsize)
+function MatrixWriter(fileName::String, expsize::Int64, datatype::DataType)
     f = open(fileName, "w")
-    mw = MatrixWriter(f, 1, 100, expsize)
+    assert(datatype in [UInt8, UInt16])
+    if datatype == UInt8 #UInt8 mode stores up to 100
+        mw = MatrixWriter(f, 1, 100, expsize, datatype)
+    elseif datatype == UInt16 #UInt16 mode stores up to 60000
+        mw = MatrixWriter(f, 1, 60000, expsize, datatype)
+    end
     writeHeader(mw)
     mw
 end
 
 function writeHeader(mw::MatrixWriter)
-   write(mw.fileStream, UInt16(mw.expsize)) 
-   write(mw.fileStream, UInt16(mw.binsize)) 
+    #Write other information.
+    write(mw.fileStream, UInt16(mw.expsize)) 
+    write(mw.fileStream, UInt16(mw.maxnum))
+    
+    #Indicate datatype.
+    if mw.datatype == UInt8
+        write(mw.fileStream, UInt16(0)) 
+    elseif mw.datatype == UInt16
+        write(mw.fileStream, UInt16(1))
+    end
 end
 
 close(mw::MatrixWriter) = close(mw.fileStream)
 
 function writeMatrix(mw::MatrixWriter, matrix::Array{Int64,2})
+    
+    skipnum = typemax(mw.datatype)-mw.maxnum
     for j in 1:size(matrix)[1]
         for i in 1:size(matrix)[2]
-            #println(matrix[j, i],":",zerocount)
             if matrix[j, i] == 0
                 mw.zerocount += 1
             elseif matrix[j, i] != 0
+                
+                # Figure out how much entry you have skipped and write it out.
                 if mw.zerocount != 1
                     temp = mw.zerocount-1
-                    _multi = floor(Int, temp/155)
-                    _const = temp%155
+                    _multi = floor(Int, temp/skipnum)
+                    _const = temp%skipnum
                     
                     for _ in 1:_multi
-                        write(mw.fileStream, UInt8(255))
+                        write(mw.fileStream, mw.datatype(typemax(mw.datatype)))
                     end
                     
                     if _const != 0
-                        write(mw.fileStream, UInt8(100+_const))
+                        write(mw.fileStream, mw.datatype(mw.maxnum+_const))
                     end
                     
-                end 
-                assert(matrix[j, i] < 101 )
-                write(mw.fileStream, UInt8(matrix[j, i]))
+                end
+                
+                # Write non-zero number.
+                if matrix[j, i] < mw.maxnum+1 
+                    # Write actual number
+                    write(mw.fileStream, mw.datatype(matrix[j, i]))
+                else
+                    println("$(matrix[j, i]) is too big. Writing $(mw.maxnum) instead")
+                    write(mw.fileStream, mw.datatype(mw.maxnum))
+                end
+                
+                # Start new zero count
                 mw.zerocount = 1
             end
         end
@@ -55,7 +81,8 @@ type MatrixReader
     # For basic matrix reading
     fileStream
     expsize::Int
-    binsize::Int
+    maxnum::Int
+    datatype::DataType
     blocksize::Int
     data::Array{Int64, 2}
     offset::Int
@@ -63,20 +90,25 @@ type MatrixReader
     # For buffering bites
     bufferpointer::Int
     buffersize::Int
-    buffer::Array{UInt8,1}
+    buffer
 end
 
-function MatrixReader(fileName::String, blocksize; buffsize=5000000)
+function MatrixReader(fileName::String, blocksize; buffsize=10000000)
     f = open(fileName)
     
     # These are read as header of file.
     _expsize = read(f, UInt16, 1)[1]
-    _binsize = read(f, UInt16, 1)[1] 
+    _maxnum = read(f, UInt16, 1)[1] 
+    _datatype = read(f, UInt16, 1)[1]
+    
+    if _datatype == 0
+        dt = UInt8
+    elseif _datatype == 1
+        dt = UInt16
+    end
     
     _buffsize = buffsize # This is 10Mb of buffer size for default
-    mr = MatrixReader(f, _expsize, _binsize, blocksize, zeros(Int64, (blocksize, _expsize)), 0, _buffsize+1, _buffsize, zeros(UInt8, _buffsize))
-    #advance!(mr)
-    #mr
+    mr = MatrixReader(f, _expsize, _maxnum, dt, blocksize, zeros(Int64, (blocksize, _expsize)), 0, _buffsize+1, _buffsize, zeros(dt, _buffsize))
 end
 
 close(mr::MatrixReader) = close(mr.fileStream)
@@ -86,11 +118,16 @@ eof(mr::MatrixReader) = eof(mr.fileStream) && mr.bufferpointer > length(mr.buffe
 function advance!(mr::MatrixReader)
     temp = zeros(Int64, (mr.blocksize, mr.expsize))
     pointer = mr.offset
-    
     while !eof(mr) && pointer < mr.blocksize*mr.expsize
         #Reload buffer
         if mr.bufferpointer > length(mr.buffer)
-            mr.buffer = read(mr.fileStream, mr.buffersize)
+            if mr.datatype == UInt8
+                mr.buffer = read(mr.fileStream, mr.buffersize)
+            elseif mr.datatype == UInt16
+                tempbuffer = read(mr.fileStream, mr.buffersize*2)
+                assert(length(tempbuffer)%2==0)
+                mr.buffer = reinterpret(UInt16, tempbuffer)
+            end
             mr.bufferpointer = 1
         end
         
@@ -99,11 +136,11 @@ function advance!(mr::MatrixReader)
         mr.bufferpointer += 1
         
         # Fill matrix
-        if value < 101
+        if value < mr.maxnum+1
             temp[ceil(Int, (pointer+1)/mr.expsize), pointer%mr.expsize+1] = value
             pointer += 1
         else
-            pointer += value-100
+            pointer += value-mr.maxnum
         end
     end
     mr.offset = pointer - mr.blocksize*mr.expsize
---
+++ test/TestMatrixReader.jl
@@ -0,0 +1,116 @@
+include("../src/MatrixReader.jl")
+
+function fakedata(fakecount, blocksize, expsize; maxval=100)
+    testdata = zeros(Int, (blocksize, expsize))
+    for _ in 1:fakecount
+        i = rand(1:blocksize)
+        j = rand(1:expsize)
+        v = rand(1:maxval)
+        testdata[i,j] = v
+    end
+    testdata
+end
+
+# Testing with 10000 random data with random sparcity, dimensions.
+
+println("For UInt8 version")
+
+for itr in 1:10000
+    expsize = rand(20:100)
+    
+    ######################
+    # Write in fake data #
+    ######################
+    mw = MatrixWriter("test.data", expsize, UInt8)
+
+    # data1
+    t1 = fakedata(rand(1:200), 100, expsize, maxval=100)
+    writeMatrix(mw, t1)
+    # data2
+    t2 = fakedata(rand(1:200), 100, expsize, maxval=100)
+    writeMatrix(mw, t2)
+    close(mw)
+    
+    #####################
+    # Read in fake data #
+    #####################
+    mr = MatrixReader("test.data", 25, buffsize=rand(10:50))
+    new = advance!(mr)
+    m1 = mr.data
+    new = advance!(mr)
+    m2 = mr.data
+    new = advance!(mr)
+    m3 = mr.data
+    new = advance!(mr)
+    m4 = mr.data
+    new = advance!(mr)
+    m5 = mr.data
+    new = advance!(mr)
+    m6 = mr.data
+    new = advance!(mr)
+    m7 = mr.data
+    new = advance!(mr)
+    m8 = mr.data
+    close(mr)
+
+    #################
+    # Test equality #
+    #################
+    x1 = vcat(t1, t2)
+    new = vcat(m1, m2, m3, m4, m5, m6, m7, m8)
+    assert(new == x1)
+    if itr%1000 == 0
+        println(itr," test passed. ", size(x1), ":", sum(x1))
+    end
+end
+
+println("For UInt16 version")
+
+for itr in 1:10000
+    expsize = rand(20:100)
+    
+    ######################
+    # Write in fake data #
+    ######################
+    mw = MatrixWriter("test.data", expsize, UInt16)
+
+    # data1
+    t1 = fakedata(rand(1:200), 100, expsize, maxval=60000)
+    writeMatrix(mw, t1)
+    # data2
+    t2 = fakedata(rand(1:200), 100, expsize, maxval=60000)
+    writeMatrix(mw, t2)
+    close(mw)
+    
+    #####################
+    # Read in fake data #
+    #####################
+    mr = MatrixReader("test.data", 25, buffsize=rand(10:50))
+    new = advance!(mr)
+    m1 = mr.data
+    new = advance!(mr)
+    m2 = mr.data
+    new = advance!(mr)
+    m3 = mr.data
+    new = advance!(mr)
+    m4 = mr.data
+    new = advance!(mr)
+    m5 = mr.data
+    new = advance!(mr)
+    m6 = mr.data
+    new = advance!(mr)
+    m7 = mr.data
+    new = advance!(mr)
+    m8 = mr.data
+    close(mr)
+
+    #################
+    # Test equality #
+    #################
+    x1 = vcat(t1, t2)
+    new = vcat(m1, m2, m3, m4, m5, m6, m7, m8)
+    assert(new == x1)
+    if itr%1000 == 0
+        println(itr," test passed. ", size(x1), ":", sum(x1))
+    end
+end
